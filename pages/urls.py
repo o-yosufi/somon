b7aea16ce263f6b53a7c29a3360924a93c73a280
o-yosufi <obaidullahyosafi@gmail.com>
2023-08-31 23:06:05 +0500

    123

--- pages/urls.py
+++ pages/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('members/', views.members, name='members'),
+    path('', views.members, name='members'),
     path('members/details/<int:id>', views.details, name='details'),
 ]
